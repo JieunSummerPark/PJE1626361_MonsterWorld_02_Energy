5496c5c498be9e9394b95b525551282ca78b95f4
JieunSummerPark <parkj2@student.swosu.edu>
2019-05-21 00:37:27 +0900

    버튼만들고 클릭

--- PJE1626361_MonsterWorld_02_EnergyDlg.cpp
+++ PJE1626361_MonsterWorld_02_EnergyDlg.cpp
@@ -65,6 +65,7 @@ BEGIN_MESSAGE_MAP(CPJE1626361MonsterWorld02EnergyDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CPJE1626361MonsterWorld02EnergyDlg::OnBnClickedButton1)
 END_MESSAGE_MAP()
 
 
@@ -153,3 +154,9 @@ HCURSOR CPJE1626361MonsterWorld02EnergyDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CPJE1626361MonsterWorld02EnergyDlg::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
